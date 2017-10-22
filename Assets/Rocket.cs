d59634ab5ceefb9d0f6a06a4ec15f7810b2df89d
Dawson <dawsonfrakes123@gmail.com>
2017-10-21 20:53:28 -0400

    Resolving Movement Bugs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -16,10 +16,11 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
@@ -33,6 +34,11 @@ public class Rocket : MonoBehaviour {
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -42,5 +48,6 @@ public class Rocket : MonoBehaviour {
         {
             transform.Rotate(-Vector3.forward);
         }
+        rigidBody.freezeRotation = false; // resume physics rotation
     }
 }
